70b082c6026016ad8a757968ba7c3483dda59595
Nouman Khan <noumankhan16859@gmail.com>
2024-10-30 22:36:55 +0530

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import NavBar from "./Components/NavBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './Components/Home';
+import About from './Components/About';
+import Projects from "./Components/Projects";
+import Contact from './Components/Contact';
+import Error404 from "./Components/Error404";
 
 function App() {
   return (
     <div className="App">
-     <NavBar/>
+      <BrowserRouter>
+        <header><NavBar /></header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,40 +1,73 @@
-import React from 'react';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  NavLink,
-} from "react-router-dom";
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "../Css/NavBar.css";
+const navLinkStyle = ({ isActive }) => {
+  return {
+    color: isActive ? "green" : "black",
+    fontWeight: isActive ? "bold" : "normal",
+    textDecoration: "none", 
+    borderBottom: isActive ? "3px solid green" : "none",
+  };
+};
+
 const NavBar = () => {
   return (
     <div>
-       <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Projects</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link " href="#">Contact</a>
-        </li>
-        
-      </ul>
+      <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid">
+          <div className="image">
+         <img src={require('../Assests/nouman.png')} alt="Nouman"  />
+         </div>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink 
+                  style={navLinkStyle} 
+                  className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}                  to={"/"}>
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink 
+                  style={navLinkStyle} 
+                  className="nav-link" 
+                  to={"/about"}>
+                  About
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink 
+                  style={navLinkStyle} 
+                  className="nav-link" 
+                  to={"/projects"}>
+                  Projects
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink 
+                  style={navLinkStyle} 
+                  className="nav-link" 
+                  to={"/contact"}>
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
-  </div>
-</nav> 
-    </div>
-  )
-}
+  );
+};
 
 export default NavBar;
