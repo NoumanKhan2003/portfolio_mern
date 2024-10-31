bd4ab35971cc2dbf7a3aae9e4a402730b5a08a96
Nouman Khan <noumankhan16859@gmail.com>
2024-10-31 17:53:23 +0530

    Error page added

--- src/Components/Error404.jsx
+++ src/Components/Error404.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-
+import React from 'react';
+import { useNavigate } from 'react-router-dom'; 
+import '../Css/Error404.css'; 
 const Error404 = () => {
-  return (
-    <div>Error404</div>
-  )
-}
+  const navigate = useNavigate(); 
 
-export default Error404
+  const handleGoBack = () => {
+    navigate(-1); 
+  };
+
+  return (
+    <div className="error-container">
+      <div className="error-box">
+        <h1 className="error-title">404</h1>
+        <p className="error-message">Oops! Page not found.</p>
+        <p className="error-description">The page you are looking for does not exist.</p>
+        <p className="error-description">You have entered the wrong URL.</p>
+        <p>If you have any query, contact Nouman Khan.</p>
+        <button onClick={handleGoBack} className="error-link">Go back</button>
+      </div>
+    </div>
+  );
+};
+
+export default Error404;
