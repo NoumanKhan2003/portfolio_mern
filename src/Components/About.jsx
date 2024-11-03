a17bb74f5dbd60300d58e643053c29f52c11271e
Nouman Khan <noumankhan16859@gmail.com>
2024-11-03 17:20:20 +0530

    Education page added

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import AboutMe from '../Components/AboutMe';
 import Skills from './Skills';
+import Education from './Education';
 const About = () => {
   return (
     <div>
 <AboutMe/>
 <Skills/>
+<Education/>
     </div>
   )
 }
---
+++ src/Components/Education.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import EducationCss from "../Css/Education.module.css";
+import collegeImg from "../Assests/Nouman Marksheet.jpg";
+import highSchoolImg from "../Assests/Nouman 12th.jpg";
+import schoolImg from "../Assests/Nouman 10th.jpg";
+
+const Education = () => {
+  const handleDownload = (fileUrl, fileName) => {
+    const link = document.createElement("a");
+    link.href = fileUrl;
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  return (
+    <div className={EducationCss.container}>
+      <div className={EducationCss.headingDiv}>
+        <h1 className={EducationCss.h1}>My Education</h1>
+      </div>
+      <div className={EducationCss.main}>
+        <div className={EducationCss.college}>
+          <h2 className={EducationCss.h2}>
+            &#9734; DeenBandhu Chhotu Ram University of Science and Technology,
+            Murthal
+          </h2>
+          <p className={EducationCss.p}>
+            B.Tech(Computer Science and Engineering){" "}
+            <p className={EducationCss.year}>(2026)</p>
+          </p>
+          <button
+            className={EducationCss.button}
+            onClick={() =>
+              handleDownload(collegeImg, "Nouman University Marksheet")
+            }
+          >
+            Download
+          </button>
+        </div>
+        <div className={EducationCss.senior}>
+          <h2 className={EducationCss.h2}>
+            &#9734; K.L. Mehta Dayanand Public Sr. Sec. School, Faridabad
+          </h2>
+          <p className={EducationCss.p}>
+            Non-Medical (CBSE) <p className={EducationCss.year}>(2022)</p>
+          </p>
+          <button
+            className={EducationCss.button}
+            onClick={() =>
+              handleDownload(highSchoolImg, "Nouman 12th Marksheet")
+            }
+          >
+            Download
+          </button>
+        </div>
+        <div className={EducationCss.junior}>
+          <h2 className={EducationCss.h2}>
+            &#9734; K.D. Sr. Sec. School, Faridabad
+          </h2>
+          <p className={EducationCss.p}>
+            HBSE <p className={EducationCss.year}>(2020)</p>
+          </p>
+          <button
+            className={EducationCss.button}
+            onClick={() => handleDownload(schoolImg, "Nouman 10th Marksheet")}
+          >
+            Download
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Education;
