ddb7b92fadc2f37abd1e15ec3833a54d64c15d85
Nouman Khan <noumankhan16859@gmail.com>
2024-11-15 16:43:40 +0530

    Home page and NavBar refactored

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,20 +1,46 @@
-import HomeCss from '../Css/Home.module.css';
+import { Navigate } from "react-router-dom";
+import HomeCss from "../Css/Home.module.css";
+import { Button } from "@mui/material";
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+  const navigate = useNavigate();
+  const handleContactClick=()=>{
+    navigate('/contact');
+  };
+  const handleAboutClick=()=>{
+    navigate('/about');
+  };
   return (
     <div className={HomeCss.main}>
-    <div className={HomeCss.left}>
-<h1 className={HomeCss.name1}>Hey, I'M </h1>
-  <h1 className={HomeCss.name2}>NOUMAN KHAN</h1>
-<p className={HomeCss.about}>I'm a aspiring full-stack web developer and software engineer, passionate about crafting innovative digital solutions. Proficient in MERN Stack. Currently exploring the world of web development through personal projects.</p>
+      <div className={HomeCss.left}>
+        <h1 className={HomeCss.name1}>Hey, I'M </h1>
+        <h1 className={HomeCss.name2} onClick={handleAboutClick}>NOUMAN KHAN</h1>
+        <p className={HomeCss.about}>
+          I'm a aspiring full-stack web developer and software engineer,
+          passionate about crafting innovative digital solutions. Proficient in
+          MERN Stack. Currently exploring the world of web development through
+          personal projects.
+        </p>
+        <div className={HomeCss.buttonDiv}>
+        <Button
+          className={HomeCss.contact}
+          variant="contained"
+          color="success"
+          size="large"
+      onClick={handleContactClick}
+        >
+          Contact Me
+        </Button>
+        </div>
+      </div>
+      <div className={HomeCss.right}>
+        <div className={HomeCss.imagediv}>
+          <img src={require("../Assests/noumanimg.jpg")} alt="Nouman Pic" onClick={handleAboutClick}/>
+        </div>
+      </div>
     </div>
-    <div className={HomeCss.right}>
-<div className={HomeCss.imagediv}>
-  <img src={require('../Assests/noumanimg.jpg')} alt="Nouman Pic" />
-</div>
-    </div>
-    </div>
-  )
-}
+  );
+};
 
 export default Home;
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,34 +1,108 @@
-import React, { useRef } from "react";
+import React, { useRef, useState ,useEffect} from "react";
 import { NavLink } from "react-router-dom";
 import NavBarCss from "../Css/NavBar.module.css";
-import { Switch } from "@mui/material";
+import { styled } from '@mui/material/styles';
+import Switch from '@mui/material/Switch';
 import { Box, Typography } from "@mui/material";
-import { useState } from "react";
+
 const navLinkStyle = ({ isActive }) => ({
-  color: isActive ? "green" : "black",
-  fontWeight: isActive ? "bold" : "normal",
+  color: isActive ? "rgb(0, 72, 0)" : "black",
+  fontWeight: isActive ? "700" : "normal",
   textDecoration: "none",
-  borderBottom: isActive ? "3px solid green" : "none",
+  borderBottom: isActive ? "4px solid rgb(0, 72, 0)" : "none",
+  transform: isActive ? "scale(1.1)" : "scale(1)",
 });
 
 const NavBar = () => {
   const navCollapseRef = useRef(null);
+  const navBarRef = useRef(null);
 
   const handleNavLinkClick = () => {
-    if (
-      navCollapseRef.current &&
-      navCollapseRef.current.classList.contains("show")
-    ) {
+    if (navCollapseRef.current && navCollapseRef.current.classList.contains("show")) {
       navCollapseRef.current.classList.remove("show");
     }
   };
 
-  const [mode, setMode] = useState("Pre");
-  const changeMode =(mode)=>{
-    setMode((prevMode) => (prevMode === "Pre" ? "Pro" : "Pre"));
-  }
+  const handleClickOutside = (event) => {
+    if (navBarRef.current && !navBarRef.current.contains(event.target)) {
+      if (navCollapseRef.current && navCollapseRef.current.classList.contains("show")) {
+        navCollapseRef.current.classList.remove("show");
+      }
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
+  const [mode, setMode] = useState(false);
+
+  const changeMode = (event) => {
+    setMode(event.target.checked);
+  };
+
+  const MaterialUISwitch = styled(Switch)(({ theme }) => ({
+    width: 62,
+    height: 34,
+    padding: 7,
+    '& .MuiSwitch-switchBase': {
+      margin: 1,
+      padding: 0,
+      transform: 'translateX(6px)',
+      transition: 'transform 0.3s ease', 
+      '&.Mui-checked': {
+        color: '#fff',
+        transform: 'translateX(22px)',
+        '& .MuiSwitch-thumb:before': {
+          backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+            '#fff',
+          )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
+        },
+        '& + .MuiSwitch-track': {
+          opacity: 1,
+          backgroundColor: '#aab4be',
+          ...theme.applyStyles('dark', {
+            backgroundColor: '#8796A5',
+          }),
+        },
+      },
+    },
+    '& .MuiSwitch-thumb': {
+      backgroundColor: '#001e3c',
+      width: 32,
+      height: 32,
+      '&::before': {
+        content: "''",
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        left: 0,
+        top: 0,
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+          '#fff',
+        )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
+      },
+      ...theme.applyStyles('dark', {
+        backgroundColor: '#003892',
+      }),
+    },
+    '& .MuiSwitch-track': {
+      opacity: 1,
+      backgroundColor: '#aab4be',
+      borderRadius: 20 / 2,
+      ...theme.applyStyles('dark', {
+        backgroundColor: '#8796A5',
+      }),
+    },
+  }));
+
   return (
-    <div>
+    <div ref={navBarRef}>
       <nav className={`${NavBarCss.navbar} navbar navbar-expand-lg`}>
         <div className={`container-fluid ${NavBarCss["container-fluid"]}`}>
           <div className={NavBarCss.imageLogo}>
@@ -48,7 +122,7 @@ const NavBar = () => {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span className="navbar-toggler-icon" ></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
           <div
             className="collapse navbar-collapse"
@@ -98,8 +172,11 @@ const NavBar = () => {
               </li>
             </ul>
             <Box className={NavBarCss.modeBtn}>
-              <Switch defaultUnChecked onChange={changeMode}/>
-              <Typography variant="button">{mode}</Typography>
+              <MaterialUISwitch
+                checked={mode}
+                onChange={changeMode}
+              />
+              <Typography variant="button" fontWeight={"bold"}>{mode ? "Pro" : "Pre"}</Typography>
             </Box>
           </div>
         </div>
