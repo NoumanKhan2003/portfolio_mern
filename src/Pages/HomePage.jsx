807c43e5345df17fd7aaa6565629a97b84d473f2
Nouman Khan <noumankhan16859@gmail.com>
2025-01-12 18:00:39 +0530

    Page Loading Component added and applied to home page

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,31 +1,55 @@
 import HomeCss from "../Css/Home.module.css";
 import { Button } from "@mui/material";
-import { useNavigate } from 'react-router-dom';
-import DownloadOutlinedIcon from '@mui/icons-material/DownloadOutlined';
-import { useContext } from "react";
-import {ModeContext} from "../Components/ModeContext";
-import Resume from "../Assests/ResumeNouman.pdf"
+import { useNavigate } from "react-router-dom";
+import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
+import { useContext, useEffect, useState } from "react";
+import { ModeContext } from "../Components/ModeContext";
+import ProgressCircle from "../Components/ProgressCircle";
+import Resume from "../Assests/ResumeNouman.pdf";
+import NoumanImg from "../Assests/noumanimg.jpg";
 const Home = () => {
   const navigate = useNavigate();
-  const handleResumeClick=(fileUrl, fileName)=>{
-      const link = document.createElement("a");
-      link.href = fileUrl;
-      link.download = fileName;
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-    };
-  const handleAboutClick=()=>{
-    navigate('/about');
-    window.scrollTo(0,0);
-  };
   const { mode } = useContext(ModeContext);
+  const [loading, setLoading] = useState(true);
+  const [themeChange, setThemeChange] = useState(false);
+
+  useEffect(() => {
+    const loadingTimeout = setTimeout(() => {
+      setLoading(false);
+    }, 300);
+    return () => clearTimeout(loadingTimeout);
+  }, []);
+
+  useEffect(() => {
+    setThemeChange(true);
+    const themeTimeout = setTimeout(() => {
+      setThemeChange(false);
+    }, 300);
+    return () => clearTimeout(themeTimeout);
+  }, [mode]);
+
+  const handleAboutClick = () => {
+    navigate("/about");
+    window.scrollTo(0, 0);
+  };
+
+  if (loading || themeChange) {
+    return <ProgressCircle />;
+  }
 
   return (
     <div className={HomeCss.main} data-theme={mode}>
       <div className={HomeCss.left}>
-        <h1 className={HomeCss.name1} data-theme={mode}>Hey, I'M </h1>
-        <h1 className={HomeCss.name2} onClick={handleAboutClick} data-theme={mode}>NOUMAN KHAN</h1>
+        <h1 className={HomeCss.name1} data-theme={mode}>
+          Hey, I'M{" "}
+        </h1>
+        <h1
+          className={HomeCss.name2}
+          onClick={handleAboutClick}
+          data-theme={mode}
+        >
+          NOUMAN KHAN
+        </h1>
         <p className={HomeCss.about} data-theme={mode}>
           I'm a aspiring full-stack web developer and software engineer,
           passionate about crafting innovative digital solutions. Proficient in
@@ -33,33 +57,38 @@ const Home = () => {
           personal projects.
         </p>
         <div className={HomeCss.buttonDiv}>
-        <Button
-          className={HomeCss.download}
-          variant="contained"
-          color="success"
-          size="large"
-          onClick={() => handleResumeClick(Resume, "Nouman's Resume")}
-          data-theme={mode}
-      startIcon={<DownloadOutlinedIcon/>}
-        >
-         Download CV
-        </Button>
+          <Button
+            className={HomeCss.download}
+            variant="contained"
+            color="success"
+            size="large"
+            href={Resume}
+            data-theme={mode}
+            startIcon={<DownloadOutlinedIcon />}
+          >
+            Download CV
+          </Button>
         </div>
       </div>
       <div className={HomeCss.right}>
         <div className={HomeCss.imagediv} data-theme={mode}>
-          <img src={require("../Assests/noumanimg.jpg")} alt="Nouman Pic" onClick={handleAboutClick} data-theme={mode}/>
+          <img
+            src={NoumanImg}
+            alt="Nouman Pic"
+            onClick={handleAboutClick}
+            data-theme={mode}
+          />
         </div>
         <Button
           className={HomeCss.download2}
           variant="contained"
           color="success"
           size="large"
-          onClick={() => handleResumeClick(Resume, "Nouman's Resume")}
+          href={Resume}
           data-theme={mode}
-      startIcon={<DownloadOutlinedIcon/>}
+          startIcon={<DownloadOutlinedIcon />}
         >
-         Download CV
+          Download CV
         </Button>
       </div>
     </div>
